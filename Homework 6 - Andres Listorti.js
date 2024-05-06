526f97017f6ce125ac7ab0409952d06f2ddd426c
andresListorti <andreslistorti@gmail.com>
2024-05-06 10:14:14 -0300

    [Corrected]

--- Homework 6 - Andres Listorti.js
+++ Homework 6 - Andres Listorti.js
@@ -52,7 +52,7 @@ function highlightKeywords(template, keywords) {
 console.log("// Testing - Task 2: Advanced Tagged Template");
 const keywords = ["JavaScript", "template", "tagged"];
 const template =
-  "Learn ${0} tagged templates to create custom ${1} literals for ${2} manipulation.";
+  "Learn \${0} tagged templates to create custom \${1} literals for \${2} manipulation.";
 const highlighted = highlightKeywords(template, keywords);
 console.log(highlighted);
 // Expected: "Learn <span class='highlight'>JavaScript</span> tagged templates to create custom <span class='highlight'>template</span> literals for <span class='highlight'>tagged</span> manipulation."
